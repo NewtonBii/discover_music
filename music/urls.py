de6705ed692de40b63fcedd624930e22f49c040c
Newton Bii <biinewton382@gmail.com>
2018-04-09 16:04:02 +0300

    Create javascript files.

---
+++ music/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+url(r'^$', views.index, name='index')
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/newton/Desktop/music_recognition_app/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
