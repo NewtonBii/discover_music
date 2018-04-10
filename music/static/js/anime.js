3dd0802b8440ef5338816a83dba2a6a2a2c76fb2
Newton Bii <biinewton382@gmail.com>
2018-04-10 11:40:20 +0300

    Add fadeIn/out animations for display box.

--- music/static/js/anime.js
+++ music/static/js/anime.js
@@ -1,12 +1,14 @@
 // Wrap every letter in a span
-$('.ml16').each(function(){
+$('.ml16').each(function() {
   $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
 
-anime.timeline({loop: true})
+anime.timeline({
+    loop: true
+  })
   .add({
     targets: '.ml16 .letter',
-    translateY: [-100,0],
+    translateY: [-100, 0],
     easing: "easeOutExpo",
     duration: 1400,
     delay: function(el, i) {
@@ -21,11 +23,37 @@ anime.timeline({loop: true})
   });
 
 // To display record animation
-$(document).ready(function(){
-    $(".record").click(function(){
-        $(".spinner").fadeIn(1000);
-    });
-    $(".stop").click(function(){
-        $(".spinner").hide();
-    });
+$(document).ready(function() {
+  $(".record").click(function() {
+    $(".recording").fadeIn(500);
+    $(".searching").fadeOut(700);
+  });
+  $(".stop").click(function() {
+    $(".recording").fadeOut(500);
+    $(".searching").fadeIn(700);
+  });
 });
+
+// Search animation
+$('.ml9 .letters').each(function() {
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({
+    loop: true
+  })
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: function(el, i) {
+      return 45 * (i + 1)
+    }
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
