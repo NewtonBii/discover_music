790155dbad81ecd2f76ca35db13a8a336fe0f6b8
Newton Bii <biinewton382@gmail.com>
2018-04-10 12:15:03 +0300

    Configure app for deployment.

--- music_search/settings.py
+++ music_search/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
