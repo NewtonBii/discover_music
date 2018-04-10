7cdf25adc1a9a8dd73ca5523da2ce8f7b1457f6d
Newton Bii <biinewton382@gmail.com>
2018-04-10 12:22:42 +0300

    Configure app for deployment.

--- music_search/settings.py
+++ music_search/settings.py
@@ -125,7 +125,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
