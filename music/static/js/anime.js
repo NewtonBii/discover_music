304ae5542812899715ef8d8b63b95188d10ef32f
Newton Bii <biinewton382@gmail.com>
2018-04-10 10:14:49 +0300

    Create animations for the introductory text.

---
+++ music/static/js/anime.js
@@ -0,0 +1,21 @@
+// Wrap every letter in a span
+$('.ml16').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml16 .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: function(el, i) {
+      return 30 * i;
+    }
+  }).add({
+    targets: '.ml16',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
